8bd8fe6434b9773c8734328e0b25babd6c9af503
ClarXo <imp_rove@naver.com>
2021-12-09 15:54:37 +0900

    fix: 불필요한 주석 제거

--- App.jsx
+++ App.jsx
@@ -3,14 +3,7 @@ import { BackHandler, PermissionsAndroid, SafeAreaView, StatusBar, StyleSheet } 
 import { WebViewContainrer } from './container';
 import { onAndroidBackHandler } from './handler';
 import { COLOR } from './style';
-/* 
-  pro - androidCanGoBack을 state로 관리할 경우 
-  자체적으로 뒤로 갈 수 있는지 여부를 관리가능
 
-  con - 매번 state 코드를 호출하기에 성능이슈
-
-  ==> 카카오 사업자 등록되고나서 로그인 페이지로 가는 현상 막히면 추가처리 안해도 됨
- */
 const App = () => {
   const webViewRef = useRef();
   useEffect(() => {
